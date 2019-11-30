de68871319beb83845e29efaa66facd2fae71645
FekketCantenel <fekketc@gmail.com>
2019-11-30 11:21:44 -0700

    removed old getAPI() code

--- ist.fn.api.js
+++ ist.fn.api.js
@@ -4,16 +4,6 @@ let todoistAPI = "https://todoist.com/api/v8/sync",
     todoistToken = "0d1383928eb454f4113b8fe921292dbc8d32ad4a",
     randUUID = "";
 
-async function getAPI2(resourceType) {
-    let resourceTypeArray = '["' + resourceType + '"]';
-
-    return (await $.getJSON(todoistAPI, {
-        token: todoistToken,
-        sync_token: "*",
-        resource_types: resourceTypeArray
-    }))[resourceType];
-}
-
 async function getAPI(path) {
     return await $.ajax({
         type: "GET",
