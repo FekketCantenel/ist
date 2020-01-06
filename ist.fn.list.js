eeb2eba54742459f62282a2863bd9f3fb771b2d0
FekketCantenel <fekketc@gmail.com>
2020-01-06 15:45:35 -0700

    added todoist:// project links on mobile #35

--- ist.fn.list.js
+++ ist.fn.list.js
@@ -110,6 +110,9 @@ function getSuggestTasksHTML(allTasks, dueTasks, projects) {
             let projectURL =
                 "<a class='suggestLink' target='_blank' href='https://todoist.com/app#agenda%2F(overdue | today) %26 %23" +
                 project.name +
+                "'>&#128279;</a>" +
+                "<a class='suggestLinkMobile' target='_blank' href='todoist://project?id=" +
+                project.id +
                 "'>&#128279;</a>";
 
             suggestTaskButton.append(badgeHTML);
