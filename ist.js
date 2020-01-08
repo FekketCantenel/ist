9324c1f6f618b324695d701469c5ddb440c3a90c
FekketCantenel <fekketc@gmail.com>
2020-01-08 16:01:21 -0700

    added Dynalist token cookie write #27

--- ist.js
+++ ist.js
@@ -9,17 +9,18 @@ import { setEvents } from "./ist.fn.event.js";
 
 $(document).ready(function() {
     async function asyncCall() {
+        let authCode = getURLParameter("code"),
+            authState = getURLParameter("state");
+
         if (Cookies.get("todoistToken") === undefined) {
-            let authCode = getURLParameter("code");
-            if (authCode) {
-                let authState = getURLParameter("state");
-                if (authState == "todoist") {
-                    $("#task").append("logging into Todoist, please wait...");
-                    let authRaw = await getAuth(authCode),
-                        authToken = authRaw.access_token;
-                    Cookies.set("todoistToken", authToken, { expires: 182 });
-                    window.location.replace("/");
-                }
+            if (authCode && authState == "todoist") {
+                $("#task").append("logging into Todoist, please wait...");
+                let authRaw = await getAuth(authCode),
+                    authToken = authRaw.access_token;
+                Cookies.set("todoistToken", authToken, {
+                    expires: 182
+                });
+                window.location.replace("/");
             } else {
                 $.get("README.md", function(readme) {
                     let converter = new showdown.Converter(),
@@ -33,50 +34,61 @@ $(document).ready(function() {
                 $("#spinner").hide();
                 return false;
             }
-        }
-
-        let todoistRawTasks = await getAPI("tasks");
-
-        // create master task objects
-        let allTasks = getAllTasks(todoistRawTasks),
-            dueTasks = getDueTasks(allTasks);
-
-        if (allTasks == "overdue") {
-            throw "overdue";
-        }
-
-        let projects = await getAPI("projects");
-
-        let highestPriorityTask = getHighestPriorityTask(dueTasks, projects);
-
-        if (highestPriorityTask) {
-            let todoistRawComments = await getAPI(
-                "comments?task_id=" + highestPriorityTask.id
-            );
-            let mainTask = getTaskHTML(
-                highestPriorityTask,
-                projects,
-                todoistRawComments
-            );
-            $("#task").append(mainTask);
         } else {
-            sessionStorage.removeItem("project.id");
-            let suggestTasks = getSuggestTasksHTML(
-                allTasks,
+            if (authCode && authState == "dynalist") {
+                $("#task").append("logging into Dynalist, please wait...");
+                Cookies.set("dynalistToken", authCode, {
+                    expires: 182
+                });
+                window.location.replace("/");
+            }
+
+            let todoistRawTasks = await getAPI("tasks");
+
+            // create master task objects
+            let allTasks = getAllTasks(todoistRawTasks),
+                dueTasks = getDueTasks(allTasks);
+
+            if (allTasks == "overdue") {
+                throw "overdue";
+            }
+
+            let projects = await getAPI("projects");
+
+            let highestPriorityTask = getHighestPriorityTask(
                 dueTasks,
                 projects
             );
 
-            if (suggestTasks[0].childElementCount > 0) {
-                $("#task").append(suggestTasks);
+            if (highestPriorityTask) {
+                let todoistRawComments = await getAPI(
+                    "comments?task_id=" + highestPriorityTask.id
+                );
+                let mainTask = getTaskHTML(
+                    highestPriorityTask,
+                    projects,
+                    todoistRawComments
+                );
+                $("#task").append(mainTask);
             } else {
-                $("#task").append("You have no due tasks!");
+                sessionStorage.removeItem("project.id");
+                let suggestTasks = getSuggestTasksHTML(
+                    allTasks,
+                    dueTasks,
+                    projects
+                );
+
+                if (suggestTasks[0].childElementCount > 0) {
+                    $("#task").append(suggestTasks);
+                } else {
+                    $("#task").append("You have no due tasks!");
+                }
             }
+
+            $("#spinner").hide();
+
+            setEvents(dueTasks, allTasks);
         }
-
-        $("#spinner").hide();
-
-        setEvents(dueTasks, allTasks);
     }
     asyncCall();
 });
