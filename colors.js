76da6338f6cc0b6c6daf890f7d4f69058b8e16b4
FekketCantenel <fekketc@gmail.com>
2022-10-10 18:14:24 -0500

    fixed todoist colors

---
+++ colors.js
@@ -0,0 +1,23 @@
+export default {
+    // https://developer.todoist.com/guides/#colors
+    berry_red: '#b8255f',
+    red: '#db4035',
+    orange: '#ff9933',
+    yellow: '#fad000',
+    olive_green: '#afb83b',
+    lime_green: '#7ecc49',
+    green: '#299438',
+    mint-green: '#6accbc',
+    teal: '#158fad',
+    sky_blue: '#14aaf5',
+    light_blue: '#96c3eb',
+    blue: '#4073ff',
+    grape: '#884dff',
+    violet: '#af38eb',
+    lavender: '#eb96eb',
+    magenta: '#e05194',
+    salmon: '#ff8d85',
+    charcoal: '#808080',
+    grey: '#b8b8b8',
+    taupe: '#ccac93'
+};
